100ec094f8aa88ededadffe53876301b1753a35c
Manuel Gherardi <manuelgherardi2004@gmail.com>
2026-07-02 11:18:37 +0200

    .

---
+++ assignment_3/tests/test_loop_invariant_noexit_but_codemotion.cpp
@@ -0,0 +1,9 @@
+int fun(int a, int b){
+    int g = 0;
+    int d = 0;
+    for(int i = 0; i<a; i++){
+        g = d + 1;
+    }
+
+    return 0;
+}
---
+++ assignment_4/tests/test_dep_neg_but_fondibili.cpp
@@ -0,0 +1,13 @@
+int fun(int a,int b){
+    int g[100];
+    int c = 0;
+    int h = 0;
+    for(int i = 0; i<a; i++){
+        c = g[i];
+    }
+    for(int i = 0; i<a; i++){
+        h = g[i+1];
+    }
+
+    return 0;
+}
